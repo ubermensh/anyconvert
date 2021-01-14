a9ef0d250a82f6216c9ea3646a75ce25c60056fe
ubermensh <lemantar.flcl@gmail.com>
2021-01-14 20:16:01 +0300

    linkage

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -1,17 +1,76 @@
 import { useRouter } from 'next/router'
 import Head from 'next/head'
+import Link from 'next/link';
+import styles from '../styles/Home.module.css'
 
-export default function Main ({total, partial, percentage}) {
+export default function Main({ total, partial, percentage, question }) {
+    const router = useRouter()
+    const url = router.query.slug[0];
+
+    function formatQuestion(index, isForTotal=false) {
+        return isForTotal ? question.replace(total, total + index +1 ): question.replace(partial, partial + index +1 )
+    }
+    function formatUrl(index, isForTotal=false) {
+        return isForTotal ? url.replace(total, total + index +1 ): url.replace(partial, partial + index +1 )
+        // return 'aaaa' + index
+    }
     return (
-        <>
-        <h2>{partial} is {percentage} of {total}</h2>
-        <ul>
-                    <li> What is X% off Y</li>
-                    <li>What % of X is Y</li>
-                    <li>X is Y percent of what number?</li>
-                    <li>X is what percent of Y</li>
-                </ul>
-        </>
+        <div className={styles.container}>
+            <main className={styles.main}>
+<h1><Link href='/'><a>main page</a></Link></h1>
+                <div className={styles.description}>
+                    <h2>{question}?</h2>
+                    <h2>{partial} is {percentage} percent of {total}</h2>
+                </div>
+                <div className={styles.grid}>
+                    <section className={styles.card}>
+                        <h3>другими формулами</h3>
+                        <ul>
+                            <li><Link href={`/what-is-${total}-of-${partial}`}><a> What is {total}% of {partial}</a></Link></li>
+                            <li><Link href={`/what-percent-of-${total}-is-${partial}`}><a> What % of {total} is {partial}</a></Link></li>
+                            <li><Link href={`/${total}-is-${partial}-of-what-number`}><a> {total} is {partial} percent of what number? </a></Link></li>
+                            <li><Link href={`/${total}-is-what-percent-of-${partial}`}><a>{total} is what percent of {partial}</a></Link></li>
+                        </ul>
+                    </section>
+                    <section className={styles.card}>
+                        <h3>Х+1</h3>
+                        <ul>
+                            {Array.from(new Array(10).keys()).map(index => (
+                                <li><Link href={formatUrl(index)}><a>{ formatQuestion(index)}</a></Link></li>
+                            ))
+                            }
+                        </ul>
+                    </section>
+                     <section className={styles.card}>
+                        <h3>Y+1</h3>
+                        <ul>
+                            {Array.from(new Array(10).keys()).map(index => (
+                                <li><Link href={formatUrl(index, true)}><a>{ formatQuestion(index, true)}</a></Link></li>
+                            ))
+                            }
+                        </ul>
+                    </section>
+                    <section className={styles.card}>
+                        <h3>Х+0.1</h3>
+                        <ul>
+                            {Array.from(new Array(10).keys()).map(index => (
+                                <li><Link href={formatUrl(index+0.1)}><a>{ formatQuestion(index+0.1)}</a></Link></li>
+                            ))
+                            }
+                        </ul>
+                    </section>
+                    <section className={styles.card}>
+                        <h3>Y+0.1</h3>
+                        <ul>
+                            {Array.from(new Array(10).keys()).map(index => (
+                                <li><Link href={formatUrl(index+0.1, true)}><a>{ formatQuestion(index+0.1, true)}</a></Link></li>
+                            ))
+                            }
+                        </ul>
+                    </section>
+                </div>
+            </main>
+        </div>
     )
 }
 
@@ -20,25 +79,25 @@ export default function Main ({total, partial, percentage}) {
 export async function getServerSideProps(context) {
     let partial, total;
     let first = true;
-    context.query.slug[0].split('-').map(
+    const q = context.query.slug[0];
+    q.split('-').map(
         (val) => {
-            if(!isNaN(val)){
-                if(first) {
+            if (!isNaN(val)) {
+                if (first) {
                     total = Number(val)
                     first = false;
                 }
-                else(
+                else (
                     partial = Number(val)
                 )
             }
         }
     )
-    console.log(total, partial);
-    // const per= (100 * partial) / total; 
+    const question = q.replaceAll('-', ' ');
     const percentage = Number(((100 * partial) / total).toFixed(2))
-    const props = {total, partial, percentage};
+    const props = { total, partial, percentage, question };
 
-  return {
-    props 
-  }
+    return {
+        props
+    }
 }
