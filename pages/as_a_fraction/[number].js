183a3f9035cf89cdfa7eaf8c745e40e7a1157603
ubermensh <lemantar.flcl@gmail.com>
2021-01-20 19:24:07 +0300

    add convenient notation of fraction

--- pages/as_a_fraction/[number].js
+++ pages/as_a_fraction/[number].js
@@ -2,20 +2,16 @@ import { useRouter } from 'next/router'
 import Link from 'next/link';
 import styles from '../../styles/Home.module.css'
 const Decimal = require('decimal.js')
-import MathJax from 'react-mathjax-preview'
 
-export default function Fraction({ number, numerator, denominator, closeValues }) {
-    // console.log(number, numerator, denominator, closeValues);
+export default function Fraction({ number, numerator, denominator, hNumerator, hDenominator, closeValues }) {
+    console.log(number, numerator, denominator, hNumerator, hDenominator );
     console.log(closeValues);
-
     return (
         <div className={styles.container}>
             <main className={styles.main}>
                 <h3><Link href="/" className={styles.card}><a>back</a></Link></h3>
-                {/* <div className={styles.description}> */}
                 <div className={styles.description}>
                     <h2>{number} as a fraction</h2>
-                    {/* <h2>{partial} is {percentage} percent of {total}</h2> */}
                 </div>
                 <div className={styles.card}>
                     What is {number} as a fraction?
@@ -25,22 +21,23 @@ How to convert {number} to a fraction:
                         <li>Write 1 as the denominator</li>
                         <li>multiply numerator and denominator by 10 as long as you get in numerator the whole number: {number}/1={numerator}/{denominator}</li>
                     </ol>
-                So, <b>{number} as a fraction is {numerator}/{denominator}</b>
+                So, <b>{number} as a fraction is {numerator}/{denominator}</b> <br/><br/>
+                    {denominator != hDenominator &&
+                        <b>or more convenient notation: {hNumerator}/{hDenominator}</b>
+                    }
+
                 </div>
                 <div className={styles.card}>
-                        <ul style={{  columnCount: 2, columnGap: "50px" }}>
-                            {Object.keys(closeValues).map((currentNumber, i) => (
-                                <li key={i}>
-                                    <span>{currentNumber} as a fraction =
-                                    {/* <math><mfrac>
-                                        <mn>{closeValues[currentNumber][0]}</mn>
-                                        <mi>{closeValues[currentNumber][1]}</mi>
-                                        </mfrac></math> */}
+                    <h3>close values</h3>
+                    <ul style={{ columnCount: 2, columnGap: "50px" }}>
+                        {Object.keys(closeValues).map((currentNumber, i) => (
+                            <li key={i}>
+                                <span>{currentNumber} as a fraction =
                                      {closeValues[currentNumber][0]}/{closeValues[currentNumber][1]}
-                                      </span>
-                                </li>
-                            ))}
-                        </ul>
+                                </span>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </main>
         </div>
@@ -48,32 +45,48 @@ How to convert {number} to a fraction:
 }
 
 export async function getServerSideProps(context) {
+
     const number = Number(context.query.number);
-    function fraction(numerator, denominator) {
+    const decimalNumber = new Decimal(number);
+
+    function fractionRecursive(numerator, denominator) {
         numerator *= 10;
         denominator *= 10;
         if (numerator % 1 != 0) {
-            return fraction(numerator, denominator)
+            return fractionRecursive(numerator, denominator)
         } else {
-            return { numerator, denominator };
+            return [numerator, denominator];
         }
     }
 
-    function getCloseValues() {
+    function fractionDecimal(num) {
+        let [n, d] = num.toFraction()
+        return [Number(n), Number(d)]
+    }
+
+
+    function getCloseValuesDecimal() {
         let res = {};
-        let currentNumber = new Decimal(number);
+        let currentNumber = decimalNumber;
         for (let i = 0; i <= 8; i++) {
             // currentNumber += 0.1;
             currentNumber = currentNumber.plus(0.1);
             // res[currentNumber] = fraction(currentNumber, 1)
-            let [num, den] = currentNumber.toFraction()
-            res[currentNumber] = [Number(num), Number(den)]
+            let [num, den] = fractionDecimal(currentNumber)
+            res[currentNumber] = [num, den]
         }
-        return res;
+        return res
     }
 
-    const { numerator, denominator } = fraction(number, 1);
-    const closeValues = getCloseValues();
+    const [numerator, denominator] = fractionRecursive(number, 1)
+    const [hNumerator, hDenominator] = fractionDecimal(decimalNumber)
+
+    const closeValues = getCloseValuesDecimal()
+
+
+    const props = { number, numerator, denominator, hNumerator, hDenominator, closeValues };
+    return { props }
+}
 
     // function numberOfZeros(n) {
     //   var c = 0;
@@ -83,12 +96,3 @@ export async function getServerSideProps(context) {
     //   }
     //   return c - 1;
     // }
-
-    //toFixed
-
-    const props = { number, numerator, denominator, closeValues };
-
-    return {
-        props
-    }
-}
--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+  
       <main className={styles.main}>
         <h1 className={styles.title}>
           web calculator
